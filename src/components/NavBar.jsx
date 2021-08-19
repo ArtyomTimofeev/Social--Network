5e75da39249ca536163916d4f46d5142bdeef709
Artyom Timofeev <nifegdeil@gmail.com>
2021-08-20 00:11:47 +0500

    базовая структура
    базовая структура

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import react from "react";
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <ul>
+        <li>
+          <a href="#">Profile</a>
+        </li>
+        <li>
+          <a href="#">Messages</a>
+        </li>
+        <li>
+          <a href="#">News</a>
+        </li>
+        <li>
+          <a href="#">Music</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
