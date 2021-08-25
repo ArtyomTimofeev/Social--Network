f1ab61e93cdb0dbbfb23524c0a777689cca4a31a
Artyom Timofeev <nifegdeil@gmail.com>
2021-08-25 14:04:29 +0500

    начало

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,8 @@
 import react from "react";
+import s from "./Profile.module.css"
 const Profile = () => {
     return(
-        <div className="content">
+        <div className={s.content}>
         <img src = 'https://images.all-free-download.com/images/graphiclarge/abstract_beach_cloud_dawn_evening_horizontal_599086.jpg'></img>
       </div>
     )
