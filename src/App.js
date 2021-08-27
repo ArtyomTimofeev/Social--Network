69f1ebdea2efa92b9cfd276e51e983f3d407f67f
Artyom Timofeev <nifegdeil@gmail.com>
2021-08-27 18:38:31 +0500

    реструктуризация 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import react from "react";
-import "./App.css";
-import Profile from "./components/Profile";
-import Header from "./components/Header";
-import NavBar from "./components/NavBar";
-
-
-
+import react from 'react';
+import './App.css';
+import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header';
+import NavBar from './components/NavBar/NavBar';
 
 const App = () => {
   return (
