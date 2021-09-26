0629be29a9d63b8b5de790b4c2a73019ab4068b4
Artyom Timofeev <nifegdeil@gmail.com>
2021-09-26 23:36:05 +0500

    shallow copy state

--- src/index.js
+++ src/index.js
@@ -6,21 +6,13 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-let rerenderEntireTree = (store) => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <Provider store={store}>
-          <App store={store} />
-        </Provider>
-      </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-};
-
-rerenderEntireTree(store);
-
-store.subscribe(() => {
-  rerenderEntireTree(store);
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
